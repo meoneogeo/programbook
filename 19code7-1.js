1a0774b1d79c8de84a91bf85f92f1f88ffbd7391
meoneogeo <30624222+meoneogeo@users.noreply.github.com>
2026-08-13 11:45:20 +0700

    Add files via upload

---
+++ 19code7-1.js
@@ -0,0 +1,183 @@
+var tourpoint = {
+  type: "FeatureCollection",
+  name: "tour_plk",
+  crs: {
+    type: "name",
+    properties: { name: "urn:ogc:def:crs:OGC:1.3:CRS84" },
+  },
+  features: [
+    {
+      type: "Feature",
+      properties: {
+        Name: "อช.ภูสอยดาว",
+        Type: "อุทยานและวนอุทยาน",
+        FID_1: 1358.0,
+        CHANGWAT: "พิษณุโลก",
+        PROVINCE_1: "Phitsanulok",
+        REGION: "ภาคเหนือ",
+        level: "B",
+      },
+      geometry: {
+        type: "MultiPoint",
+        coordinates: [[100.947166666666661, 17.69645]],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        Name: "อช.น้ำตกชาติตระการ",
+        Type: "อุทยานและวนอุทยาน",
+        FID_1: 1357.0,
+        CHANGWAT: "พิษณุโลก",
+        PROVINCE_1: "Phitsanulok",
+        REGION: "ภาคเหนือ",
+        level: "B",
+      },
+      geometry: {
+        type: "MultiPoint",
+        coordinates: [[100.677166666666665, 17.301816666666667]],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        Name: "น้ำตกชาติตระการ",
+        Type: "ธรรมชาติ",
+        FID_1: 1362.0,
+        CHANGWAT: "พิษณุโลก",
+        PROVINCE_1: "Phitsanulok",
+        REGION: "ภาคเหนือ",
+        level: "B",
+      },
+      geometry: {
+        type: "MultiPoint",
+        coordinates: [[100.6831, 17.2981]],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        Name: "น้ำตกนาจาน",
+        Type: "ธรรมชาติ",
+        FID_1: 1364.0,
+        CHANGWAT: "พิษณุโลก",
+        PROVINCE_1: "Phitsanulok",
+        REGION: "ภาคเหนือ",
+        level: "D",
+      },
+      geometry: {
+        type: "MultiPoint",
+        coordinates: [[100.691283333333331, 17.3185]],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        Name: "อช.แก่งเจ็ดแคว",
+        Type: "อุทยานและวนอุทยาน",
+        FID_1: 1355.0,
+        CHANGWAT: "พิษณุโลก",
+        PROVINCE_1: "Phitsanulok",
+        REGION: "ภาคเหนือ",
+        level: "D",
+      },
+      geometry: {
+        type: "MultiPoint",
+        coordinates: [[100.44016666666667, 17.1411]],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        Name: "น้ำตกน้ำโจน",
+        Type: "ธรรมชาติ",
+        FID_1: 1365.0,
+        CHANGWAT: "พิษณุโลก",
+        PROVINCE_1: "Phitsanulok",
+        REGION: "ภาคเหนือ",
+        level: "D",
+      },
+      geometry: {
+        type: "MultiPoint",
+        coordinates: [[100.416783333333328, 17.172966666666667]],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        Name: "น้ำตกแก่งไทร",
+        Type: "ธรรมชาติ",
+        FID_1: 733.0,
+        CHANGWAT: "มุกดาหาร",
+        PROVINCE_1: "Mukdahan",
+        REGION: "ภาคตะวันออกเฉียงเหนือ",
+        level: "D",
+      },
+      geometry: {
+        type: "MultiPoint",
+        coordinates: [[100.43675, 17.143116666666668]],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        Name: "อช.ภูหินร่องกล้า",
+        Type: "อุทยานและวนอุทยาน",
+        FID_1: 1359.0,
+        CHANGWAT: "พิษณุโลก",
+        PROVINCE_1: "Phitsanulok",
+        REGION: "ภาคเหนือ",
+        level: "A",
+      },
+      geometry: {
+        type: "MultiPoint",
+        coordinates: [[100.993266666666671, 17.00305]],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        Name: "น้ำตกผาลาด",
+        Type: "ธรรมชาติ",
+        FID_1: 603.0,
+        CHANGWAT: "เลย",
+        PROVINCE_1: "Loei",
+        REGION: "ภาคตะวันออกเฉียงเหนือ",
+        level: "C",
+      },
+      geometry: {
+        type: "MultiPoint",
+        coordinates: [[100.938583333333327, 17.032866666666667]],
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        Name: "ลานหินแตก",
+        Type: "ธรรมชาติ",
+        FID_1: 611.0,
+        CHANGWAT: "เลย",
+        PROVINCE_1: "Loei",
+        REGION: "ภาคตะวันออกเฉียงเหนือ",
+        level: "A",
+      },
+      geometry: { type: "MultiPoint", coordinates: [[100.99, 17.00615]] },
+    },
+    {
+      type: "Feature",
+      properties: {
+        Name: "ลานหินปุ่ม",
+        Type: "ธรรมชาติ",
+        FID_1: 1370.0,
+        CHANGWAT: "พิษณุโลก",
+        PROVINCE_1: "Phitsanulok",
+        REGION: "ภาคเหนือ",
+        level: "B",
+      },
+      geometry: {
+        type: "MultiPoint",
+        coordinates: [[100.99705, 16.9881]],
+      },
+    },
+  ],
+};
